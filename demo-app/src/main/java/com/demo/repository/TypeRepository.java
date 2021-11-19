bbdf9022e86d3d30980aa7de15bb33a9b5c32c37
david <davidakiss@gmail.com>
2021-11-18 22:29:36 -0500

    fixed query method in repository

--- demo-app/src/main/java/com/demo/repository/TypeRepository.java
+++ demo-app/src/main/java/com/demo/repository/TypeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 //#{{#each graphql.json.type.Query}}
 //#{{#if (eval type '==' ../_key)}}
 //#    // use query that best maps to the generated method:
-//#    @Query("from {{_key}}")
+//#    @Query("from {{type}}")
 //#    {{#if (eval array '==' true)}}List<{{/if}}{{type}}{{#if (eval this.array '==' true)}}>{{/if}} {{@key}}({{#join args}}{{map ../../typeMappings.json type}} {{@key}}{{/join}});
 //#{{/if}}
 //#{{/each}}
