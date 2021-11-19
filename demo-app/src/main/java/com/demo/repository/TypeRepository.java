0fb18108b28ee50af68bc105541f8774d9f6e840
david <davidakiss@gmail.com>
2021-11-18 22:27:29 -0500

    added Query annotation in repository

--- demo-app/src/main/java/com/demo/repository/TypeRepository.java
+++ demo-app/src/main/java/com/demo/repository/TypeRepository.java
@@ -1,11 +1,14 @@
 package com.demo.repository;
 
 import com.demo.domain.Type;
+import javax.persistence.Query;
 import org.springframework.data.repository.CrudRepository;
 
 //#public interface {{_key}}Repository extends CrudRepository<{{_key}}, {{idClass}}> {
 //#{{#each graphql.json.type.Query}}
 //#{{#if (eval type '==' ../_key)}}
+//#    // use query that best maps to the generated method:
+//#    @Query("from {{_key}}")
 //#    {{#if (eval array '==' true)}}List<{{/if}}{{type}}{{#if (eval this.array '==' true)}}>{{/if}} {{@key}}({{#join args}}{{map ../../typeMappings.json type}} {{@key}}{{/join}});
 //#{{/if}}
 //#{{/each}}
