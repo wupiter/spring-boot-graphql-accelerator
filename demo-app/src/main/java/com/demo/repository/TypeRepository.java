1e25ac91c55e17d72beb5d25890f1f439348da67
david <davidakiss@gmail.com>
2021-11-18 22:23:18 -0500

    added query methods to repository

--- demo-app/src/main/java/com/demo/repository/TypeRepository.java
+++ demo-app/src/main/java/com/demo/repository/TypeRepository.java
@@ -4,4 +4,9 @@ import com.demo.domain.Type;
 import org.springframework.data.repository.CrudRepository;
 
 //#public interface {{_key}}Repository extends CrudRepository<{{_key}}, {{idClass}}> {
+//#{{#each graphql.json.type.Query}}
+//#{{#if (eval type '==' ../_key)}}
+//#    {{#if (eval array '==' true)}}List<{{/if}}{{type}}{{#if (eval this.array '==' true)}}>{{/if}} {{@key}}({{#join args}}{{map ../../typeMappings.json type}} {{@key}}{{/join}});
+//#{{/if}}
+//#{{/each}}
 //#}
