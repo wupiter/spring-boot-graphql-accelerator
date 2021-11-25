f7f504ef4f8542fac384a62284bc3afd2310917e
david <davidakiss@gmail.com>
2021-11-24 22:52:34 -0500

    switching to new graphql parser

--- demo-app/src/main/java/com/demo/controller/GraphQLController.java
+++ demo-app/src/main/java/com/demo/controller/GraphQLController.java
@@ -8,22 +8,22 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequiredArgsConstructor
 public class GraphQLController {
-//#{{#each graphql.json.type}}
-//#{{#if (eval @key 'notIn' (array 'Query' 'Mutation' 'Subscription'))}}
+//#{{#each graphql.json.definitions}}
+//#{{#if (eval @key 'notIn' (array 'String' 'Boolean' 'Int'))}}
 //#    private final {{@key}}Repository {{lowerCase @key}}Repository;
 //#{{/if}}
 //#{{/each}}
 
-//#{{#each graphql.json.type.Query}}
+//#{{#each graphql.json.properties.Query.properties}}
 //#    @QueryMapping
-//#    public {{#if (eval this.array '==' true)}}List<{{/if}}{{type}}{{#if (eval this.array '==' true)}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#    public {{#if (eval this.properties.return.type '==' 'array')}}List<{{/if}}{{type}}{{#if (eval this.properties.return.type '==' 'array')}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return {{lowerCase type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
 //#    }
 //#{{/each}}
 //#
 //#{{#each graphql.json.type.Mutation}}
 //#    @MutationMapping
-//#    public {{#if (eval this.array '==' true)}}List<{{/if}}{{type}}{{#if (eval this.array '==' true)}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#    public {{#if (eval this.properties.return.type '==' 'array')}}List<{{/if}}{{type}}{{#if (eval this.properties.return.type '==' 'array')}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return {{lowerCase type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
 //#    }
 //#{{/each}}
