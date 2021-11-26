6ccb2623cda4b96d868d2e07abf2e755ded4cb7e
david <davidakiss@gmail.com>
2021-11-26 12:55:43 -0500

    fixing queries in controller

--- demo-app/src/main/java/com/demo/controller/GraphQLController.java
+++ demo-app/src/main/java/com/demo/controller/GraphQLController.java
@@ -9,19 +9,25 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class GraphQLController {
 //#{{#each graphql.json.definitions}}
-//#{{#if (eval @key 'notIn' (array 'String' 'Boolean' 'Int'))}}
+//#{{#if (eval @key 'notIn' (array 'String' 'Boolean' 'Int' 'ID'))}}
 //#    private final {{@key}}Repository {{lowerCase @key}}Repository;
 //#{{/if}}
 //#{{/each}}
 
 //#{{#each graphql.json.properties.Query.properties}}
 //#    @QueryMapping
-//#    public {{#if (eval this.properties.return.type '==' 'array')}}List<{{/if}}{{type}}{{#if (eval this.properties.return.type '==' 'array')}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
-//#        return {{lowerCase type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
+//#{{#if (eval properties.return.type '==' 'array')}}
+//#    public List<{{properties.return.items.anyOf[0]['$ref']}}> {{@key}}({{#join properties.arguments.properties}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#        return {{lowerCase properties.return.items.anyOf[0]['$ref']}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
 //#    }
+//#{{else}}
+//#    public {{properties.return.type}} {{@key}}({{#join properties.arguments.properties}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#        return {{lowerCase properties.return.type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
+//#    }
+//#{{/if}}
 //#{{/each}}
 //#
-//#{{#each graphql.json.type.Mutation}}
+//#{{#each graphql.json.properties.Mutation.properties}}
 //#    @MutationMapping
 //#    public {{#if (eval this.properties.return.type '==' 'array')}}List<{{/if}}{{type}}{{#if (eval this.properties.return.type '==' 'array')}}>{{/if}} {{@key}}({{#join args}}@Argument {{map ../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return {{lowerCase type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
