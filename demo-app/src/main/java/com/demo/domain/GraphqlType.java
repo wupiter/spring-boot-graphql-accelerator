ec963166c9655da1f00c1c4f7a50f21da2d08c4d
david <davidakiss@gmail.com>
2021-12-06 22:06:12 -0500

    removed typeMappings debugging

--- demo-app/src/main/java/com/demo/domain/GraphqlType.java
+++ demo-app/src/main/java/com/demo/domain/GraphqlType.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 //#{{/if}}
 
-//a{{{toJSON this}}}
 //#{{#graphqlType _item _key}}
 //#{{#if (eval enum '==' true)}}
 //#public enum GraphqlType {
@@ -17,7 +16,6 @@ import org.hibernate.annotations.GenericGenerator;
 //#@Entity
 //#@Data
 //#public class {{name}} {
-//b{{{toJSON this}}}
 //#{{#each fields}}
 //#{{#if (eval type '==' 'ID')}}
 //#    @Id
@@ -31,12 +29,6 @@ import org.hibernate.annotations.GenericGenerator;
 //#{{#if (eval array '==' true)}}
 //#    @OneToMany
 //#{{/if}}
-//{{map ../../../typeMappings.json type}}
-//c{{{toJSON this}}}
-//1{{{toJSON ../typeMappings.json}}}
-//2{{{toJSON ../../typeMappings.json}}}
-//3{{{toJSON ../../../typeMappings.json}}}
-//4{{{toJSON ../../../../typeMappings.json}}}
 //#    private {{#if (eval array '==' true)}}List<{{/if}}{{#if (map ../../typeMappings.json type)}}{{map ../../typeMappings.json type}}{{else}}{{type}}{{/if}}{{#if (eval array '==' true)}}>{{/if}} {{@key}};
 //#{{/each}}
 //#}
