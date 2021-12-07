28a2ae4aea4617b6f48f1659b0d9e300a6633bf9
david <davidakiss@gmail.com>
2021-12-06 21:53:51 -0500

    debugging typeMappings

--- demo-app/src/main/java/com/demo/domain/GraphqlType.java
+++ demo-app/src/main/java/com/demo/domain/GraphqlType.java
@@ -30,9 +30,10 @@ import org.hibernate.annotations.GenericGenerator;
 //#    @OneToMany
 //#{{/if}}
 //{{map ../../../typeMappings.json type}}
-//1{{{../typeMappings.json}}}
-//2{{{../../typeMappings.json}}}
-//3{{{../../../typeMappings.json}}}
+//{{toJSON this}}
+//1{{{toJSON ../typeMappings.json}}}
+//2{{{toJSON ../../typeMappings.json}}}
+//3{{{toJSON ../../../typeMappings.json}}}
 //#    private {{#if (eval array '==' true)}}List<{{/if}}{{#if (map ../../typeMappings.json type)}}{{map ../../typeMappings.json type}}{{else}}{{type}}{{/if}}{{#if (eval array '==' true)}}>{{/if}} {{@key}};
 //#{{/each}}
 //#}
