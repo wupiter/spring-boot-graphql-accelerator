4f54e97dfd1e1c65350d329a35f33a87ae1c9fa0
david <davidakiss@gmail.com>
2021-12-09 22:23:05 -0500

    added readme and JpaConfig class

---
+++ demo-app/src/main/java/com/demo/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+public class JpaConfig {
+}
