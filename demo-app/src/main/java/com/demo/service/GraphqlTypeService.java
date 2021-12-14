e34eca5b0902778a2b4af74796b9b827eaa7f9dc
david <davidakiss@gmail.com>
2021-12-13 21:23:21 -0500

    fixingtype filter in repo and service class

--- demo-app/src/main/java/com/demo/service/GraphqlTypeService.java
+++ demo-app/src/main/java/com/demo/service/GraphqlTypeService.java
@@ -19,7 +19,7 @@ public class GraphqlTypeService {
 
 //#{{#each graphql.json.properties.Query.properties}}
 //#{{#graphqlOp this @key}}
-//#{{#if (eval type '==' ../_key)}}
+//#{{#if (eval return.type '==' ../_key)}}
 //#    @Transactional(readOnly = true)
 //#    public {{#if (eval return.array '==' true)}}List<{{/if}}{{return.type}}{{#if (eval return.array '==' true)}}>{{/if}} {{@key}}({{#join args}}{{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return {{lowerCase this.return.type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
@@ -30,7 +30,7 @@ public class GraphqlTypeService {
 //#
 //#{{#each graphql.json.properties.Mutation.properties}}
 //#{{#graphqlOp this @key}}
-//#{{#if (eval type '==' ../_key)}}
+//#{{#if (eval return.type '==' ../_key)}}
 //#    public {{#if (eval this.return.array '==' true)}}List<{{/if}}{{this.return.type}}{{#if (eval this.return.array '==' true)}}>{{/if}} {{this.name}}({{#join this.args}}{{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return {{lowerCase this.return.type}}Repository.{{@key}}({{#join args}}{{@key}}{{/join}});
 //#    }
