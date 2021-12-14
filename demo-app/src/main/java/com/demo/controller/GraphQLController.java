d51d81ddef460b1bfddfc70177e61c97e8cef9ab
david <davidakiss@gmail.com>
2021-12-14 00:41:01 -0500

    removing ResponseEntity from controller as it's not supported

--- demo-app/src/main/java/com/demo/controller/GraphQLController.java
+++ demo-app/src/main/java/com/demo/controller/GraphQLController.java
@@ -5,7 +5,6 @@ import com.demo.service.*;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.*;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -20,7 +19,7 @@ public class GraphQLController {
 //#{{#each graphql.json.properties.Query.properties}}
 //#{{#graphqlOp this @key}}
 //#    @QueryMapping
-//#    public ResponseEntity<{{#if (eval this.return.array '==' true)}}List<{{/if}}{{this.return.type}}{{#if (eval this.return.array '==' true)}}>{{/if}}> {{this.name}}({{#join this.args}}@Argument {{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#    public {{#if (eval this.return.array '==' true)}}List<{{/if}}{{this.return.type}}{{#if (eval this.return.array '==' true)}}>{{/if}} {{this.name}}({{#join this.args}}@Argument {{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return ResponseEntity.ok({{lowerCase this.return.type}}Service.{{@key}}({{#join args}}{{@key}}{{/join}}));
 //#    }
 //#
@@ -29,7 +28,7 @@ public class GraphQLController {
 //#{{#each graphql.json.properties.Mutation.properties}}
 //#{{#graphqlOp this @key}}
 //#    @MutationMapping
-//#    public ResponseEntity<{{#if (eval this.return.array '==' true)}}List<{{/if}}{{this.return.type}}{{#if (eval this.return.array '==' true)}}>{{/if}}> {{this.name}}({{#join this.args}}@Argument {{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
+//#    public {{#if (eval this.return.array '==' true)}}List<{{/if}}{{this.return.type}}{{#if (eval this.return.array '==' true)}}>{{/if}} {{this.name}}({{#join this.args}}@Argument {{map ../../../typeMappings.json type}} {{@key}}{{/join}}) {
 //#        return ResponseEntity.ok({{lowerCase this.return.type}}Service.{{@key}}({{#join args}}{{@key}}{{/join}}));
 //#    }
 //#
